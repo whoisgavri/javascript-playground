74c975da32b29fbebeb82f86e861430287bfa5dc
whoisgavri <epartida@alumnos.cmtepa.edu.mx>
2022-10-24 15:56:22 -0500

    added a button

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 window.onload = () => {
     console.log('This is your first program executing!');
 }
+const para = document.querySelector('div');
+para.addEventListener('click', updateName);
+function updateName() {
+ let name = prompt('Enter a new name');
+ para.textContent = 'Dancer 1: ' + name;
+}
